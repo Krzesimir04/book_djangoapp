3d890d3111e32e5f807eaf5c9207fa884b11f627
Krzesimir04 <krzesimirr2004@gmail.com>
2023-01-04 17:45:00 +0100

    changes in settings (whitenoise, etc.), requirements

--- book_proj/book_proj/settings.py
+++ book_proj/book_proj/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,13 @@ SECRET_KEY = 'django-insecure-@_ja9hon@r*ljh=#4$*92xu@n03d)_coznqurcofm-m8gr52ld
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
+
+SECURE_SSL_REDIRECT=False
+SESSION_COOKIE_SECURE=False
+CSRF_COOKIE_SECURE=False
+CSRF_TRUSTED_ORIGINS=['https://bookdjangoapp-production.up.railway.app']
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,10 +43,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'book_app',
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -105,6 +113,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[BASE_DIR / 'static']
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
