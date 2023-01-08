55533dd22259089a60da085065573afe00a717b2
Krzesimir04 <krzesimirr2004@gmail.com>
2023-01-08 09:46:32 +0100

    styles

--- .history/book_proj/book_app/models_20221230114648.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-from .validators import *
-# Create your models here.
-class Visit(models.Model):
-    User=models.ForeignKey(User,on_delete=models.CASCADE, null=True)
-    Day=models.DateField(validators=[visit_day_validator])
-    Term=models.TimeField(null=True, validators=[visit_term_validator])
-    Visit_length=models.FloatField(null=False)
